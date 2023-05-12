de4b8f418d91dffc91ee3be9ca1498567fbc6e87
JIMS-Innovations <jimsufficiency@gmail.com>
2023-05-12 16:42:28 +0100

    Update

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -30,6 +30,5 @@ void test_validate_my_username()
     const char* name_2 = malloc_username_from_conf_file();
     TEST_ASSERT_EQUAL_STRING_MESSAGE(name_1, name_2, "Validate Username");
 
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
 
 }
